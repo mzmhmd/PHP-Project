dde44a199f4ed1b8ee740cad41baf4cd6ff04409
Muhammad Zakeer Muhammad <162731983+mzmhmd@users.noreply.github.com>
2024-10-13 18:39:57 +0530

    initial commit

---
+++ about.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lanka Pc Build</title>
+    <link rel="stylesheet" href="css/about.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;400;500;600;700;800;900&family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="navbar">
+            <div class="logo">
+                <a href="index.php"><img src="img/logo1-x2.png"></a>
+            </div>
+            <nav>
+                <ul id="MenuItems">
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="product.php?status=categoryView&&id=1">Products</a></li>
+                    <li><a href="about.php">About</a></li>
+                    <li><a href="contact.php">Contact</a></li>
+                    <li><a href="account.php">Account</a></li>
+                </ul>
+            </nav>
+            <a href="addtoCart.php"><img src="img/cart bag.png" width="30px" height="30px"></a>
+
+
+        </div>
+    </div>
+
+    <!-- ----------------about us------------------ -->
+    <div class="responsive-container-block bigContainer">
+        <div class="responsive-container-block Container bottomContainer">
+            <div class="allText bottomText">
+                <p class="text-blk headingText">
+                    About
+                </p>
+                <p class="text-blk subHeadingText">
+                    We build to your PC
+                </p>
+                <p class="text-blk description">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fermentum pulvinar ullamcorper suspendisse ac eget. Pellentesque tempus leo in ullamcorper quis vestibulum ligula elementum ut. Congue in dignissim tincidunt ut dolor eu. Mi, eget posuere vitae sed purus nisl lorem.
+                </p>
+                <a>
+                    <button class="explore">
+                        View More
+                    </button>
+                </a>
+            </div>
+            <div class="videoContainer">
+                <img class="mainVideo" src="img/build pc.jpg">
+                <img class="dotsImg image-block" src="img/cw3.svg">
+            </div>
+        </div>
+    </div>
+
+
+    <!-- ----------------footer------------------- -->
+    <div class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="footer-col-1">
+                    <h3>Download Our App</h3>
+                    <p>Download App For Android and Ios Mobile Phone.</p>
+                    <div class="app-logo">
+                        <img src="img/play-store.png" alt="">
+                        <img src="img/app-store.png" alt="">
+                    </div>
+                </div>
+                <div class="footer-col-2">
+                    <img src="img/logo-color-removebg-preview.png" alt="">
+                    <p>Lanka Pc Build (PVT) Ltd is an innovative ICT product distributor and service provider which established in 2001.</p>
+                </div>
+                <div class="footer-col-3">
+                    <h3>useful Links</h3>
+                    <ul>
+                        <li>Coupons</li>
+                        <li>Blog Post</li>
+                        <li>Return Policy</li>
+                        <li>Join Affiliate</li>
+                    </ul>
+                </div>
+                <div class="footer-col-4">
+                    <h3>Follow us</h3>
+                    <ul>
+                        <li>Facebook</li>
+                        <li>Twitter</li>
+                        <li>Instagram</li>
+                        <li>Youtube</li>
+                    </ul>
+                </div>
+            </div>
+            <hr>
+            <p class="copyright">Copyright © 2020-23 PC Builder™</p>
+        </div>
+    </div>
+
+
+</body>
+
+</html>
